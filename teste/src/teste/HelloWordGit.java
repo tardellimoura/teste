6e1f5454c2bb5d55fab0f523c3228d538987b92b
Agence <Agence@Agence-PC>
2014-12-01 10:41:27 -0200

    teste de primeiro commit

---
+++ teste/src/teste/HelloWordGit.java
@@ -0,0 +1,12 @@
+package teste;
+
+public class HelloWordGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("testando olá mundo");
+
+	}
+
+}
