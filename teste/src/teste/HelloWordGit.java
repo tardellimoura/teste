0b6f2270d8b07d9cf3944cfad635f2c2e6dce8b6
Agence <Agence@Agence-PC>
2014-12-01 10:51:19 -0200

    teste

--- teste/src/teste/HelloWordGit.java
+++ teste/src/teste/HelloWordGit.java
@@ -4,8 +4,8 @@ public class HelloWordGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("testando olá mundo");
+		String msg = new TesteCommit("teste").getMsgTesteCommit();
+		System.out.println(msg);
 
 	}
 
