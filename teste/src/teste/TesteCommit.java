b1428c7b042bd8b452fa8d759d3881811575de96
Agence <Agence@Agence-PC>
2014-12-01 10:51:44 -0200

    fsa

---
+++ teste/src/teste/TesteCommit.java
@@ -0,0 +1,22 @@
+package teste;
+
+public class TesteCommit {
+
+	private String msgTesteCommit = "asfasdfasfsafas";
+
+	public String getMsgTesteCommit() {
+		return msgTesteCommit;
+	}
+
+	public void setMsgTesteCommit(String msgTesteCommit) {
+		this.msgTesteCommit = msgTesteCommit;
+	}
+
+	public TesteCommit(String msgTesteCommit) {
+		super();
+		this.msgTesteCommit = msgTesteCommit;
+	}
+	
+	
+	
+}
